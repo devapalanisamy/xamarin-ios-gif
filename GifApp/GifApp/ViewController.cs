42abeda8fda10dff55e3ee4b0ade9324b7304985
Deva Palanisamy <deva.palanisamy@replgroup.com>
2015-10-24 20:08:10 +0100

    initial commit

---
+++ GifApp/GifApp/ViewController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using UIKit;
+using CoreGraphics;
+
+namespace GifApp
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			GifImageView.Image = UIImage.FromFile ("Gif Images/tmp-1.png");
+
+			GifImageView.AnimationImages = new UIImage[]{
+				UIImage.FromFile ("Gif Images/tmp-0.png")
+				,UIImage.FromFile ("Gif Images/tmp-1.png")
+				,UIImage.FromFile ("Gif Images/tmp-2.png")
+				,UIImage.FromFile ("Gif Images/tmp-3.png")
+				,UIImage.FromFile ("Gif Images/tmp-4.png")
+				,UIImage.FromFile ("Gif Images/tmp-5.png")
+			};
+
+			GifImageView.AnimationRepeatCount = 0;
+			GifImageView.AnimationDuration = .5;
+			GifImageView.StartAnimating();
+
+		}
+	}
+}
+
